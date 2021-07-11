a39baa1cad14d9e11d0a9e354ea845be4b5c0c95
wagner junior <wagnerjunior645@hotmail.com>
2021-07-11 12:35:23 -0300

    Segunda aula giselle

---
+++ aula 2/index.js
@@ -0,0 +1,106 @@
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// let sexo = 'feminino';
+// console.log(sexo);
+// let corDosOlhos = 'castanho';
+// console.log(corDosOlhos);
+// let nome = 'giselle';
+// console.log(nome);
+// let cidade = 'aracaju';
+// console.log(cidade);
+// let peso = 50;
+// console.log(peso);
+// let tomouVacina = true;
+// console.log(tomouVacina);
+// let temCachorro = true;
+// console.log(temCachorro);
+// let gostaDeSol = false;
+// console.log(gostaDeSol);
+// let solteira = false;
+// console.log(solteira);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let nome = 'andrezza';
+let idade = 40;
+let tomouVacina = true;
+let sexo = 'f';
+let altura = 165;
+
+let nome1 = 'solange';
+let idade1 = 45;
+let tomouVacina1 = true;
+let sexo1 = 'f';
+let altura1 = 170;
+
+let nome2 = 'elis';
+let idade2 = 46;
+let tomouVacina2 = false
+let sexo2 = 'f';
+let altura2 = 171;
+
+console.log(nome);
+console.log(idade);
+console.log(tomouVacina);
+console.log(sexo);
+console.log(altura);
+
+console.log('\n')
+
+console.log(nome1);
+console.log(idade1);
+console.log(tomouVacina1);
+console.log(sexo1);
+console.log(altura1);
+
+console.log('\n')
+
+console.log(nome2);
+console.log(idade2);
+console.log(tomouVacina2);
+console.log(sexo2);
+console.log(altura2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
